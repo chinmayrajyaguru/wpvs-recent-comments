fc4454352da7c376df887563e9fbc03493c33cc4
Chinmay Rajyaguru <info@inisolution.com>
2016-10-12 21:07:20 +0530

    Create wpvs-recent-comments.php

---
+++ wpvs-recent-comments.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: WordPress vertical scroll recent comments
+Description: WordPress vertical scroll recent comments plugin will scroll the recent post comment vertically (bottom to top) in the widget.
+Author: Chinmay Rajyaguru
+Author URI: http://inisolution.com/
+Plugin URI: http://inisolution.com/blog/
+Version: 1.0
+Tags: wordpress, vertical, scroll, recent, comments, comment, widget, plugin,
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wpvs-recent-comments
+*/
+
+
+
+?>
